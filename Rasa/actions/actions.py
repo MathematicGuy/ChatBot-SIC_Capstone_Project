5d3722445c51f3d4c3ff02ca0787a872714242c8
Sukmadi <nhatthanhdinh482@gmail.com>
2024-08-11 11:18:41 +0700

    Simplify Chatbot Development with Form Actions

--- Rasa/actions/actions.py
+++ Rasa/actions/actions.py
@@ -7,22 +7,29 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionSayData(Action):
+
+    def name(self) -> Text:
+        return "action_say_data"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        tech_number = tracker.get_slot("tech_number")
+        name = tracker.get_slot("name")
+        city = tracker.get_slot("city")
+        
+        if not tech_number:
+            dispatcher.utter_message(text="I'm sorry, I couldn't find your information.")
+        else:
+            dispatcher.utter_message(text=f"Hi {name}, I see that your city is {city} and your neural id is {tech_number}.")
+
+        return []
 
